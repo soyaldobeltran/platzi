24b5f9cadf6f4d19530726f7482f8107ee70169b
soyaldobeltran <andresaldo45@gmail.com>
2023-10-11 12:21:45 -0500

    Nuevas funciones
    Ataques, mostrar mensajes en el DOM

--- curso_de_programacion_basica/mokepon/js/mokepon.js
+++ curso_de_programacion_basica/mokepon/js/mokepon.js
@@ -1,7 +1,17 @@
+let ataqueJugador 
+let ataqueEnemigo
 
 function startGame(){
     let btnMascotaJugador = document.getElementById('btn-mascota');
     btnMascotaJugador.addEventListener('click', checkMascotaJugador);
+
+    let btnFuego = document.getElementById('btn-fuego');
+    btnFuego.addEventListener('click', ataqueFuego);
+    let btnTierra = document.getElementById('btn-tierra');
+    btnTierra.addEventListener('click', ataqueTierra);
+    let btnAgua = document.getElementById('btn-agua');
+    btnAgua.addEventListener('click', ataqueAgua);
+    
 }
 function checkMascotaJugador(){
     let hipodoge = document.getElementById('hipodoge')
@@ -18,6 +28,73 @@ function checkMascotaJugador(){
     }else {
         alert('Aún no has seleccionado a una Mascota')
     }
+
+    checkMascotaEnemigo()
+}
+
+function checkMascotaEnemigo(){
+    let mascotaAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+    if (mascotaAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (mascotaAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    }else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+function ataqueFuego(){
+    ataqueJugador = 'Fuego'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua(){
+    ataqueJugador = 'Agua'
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra(){
+    ataqueJugador = 'Tierra'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    
+    if (ataqueAleatorio == 1){
+        ataqueEnemigo = 'Fuego'
+    } else if (ataqueAleatorio == 2){
+        ataqueEnemigo = 'Agua'
+    }else{
+        ataqueEnemigo = 'Tierra'
+    }
+
+    combate()
+}
+
+function combate(){
+    if(ataqueEnemigo == ataqueJugador){
+        crearMensaje('Empate')
+    } else if(ataqueJugador == 'Fuego' && ataqueEnemigo == 'Tierra'){
+        crearMensaje('Ganaste')
+    } else if (ataqueJugador == 'Agua' && ataqueEnemigo == 'Fuego'){
+        crearMensaje('Ganaste')
+    } else if (ataqueJugador == 'Tierra' && ataqueEnemigo == 'Agua'){
+        crearMensaje('Ganaste')
+    } else {
+        crearMensaje('Perdiste')
+    }
+}
+
+function crearMensaje(resultado){
+    let sectionMensajes = document.getElementById('mensajes')
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Tu macota atacó con ' + ataqueJugador+ ', la mascota del enemigo atacó con ' + ataqueEnemigo + ' -' + resultado
+    sectionMensajes.appendChild(parrafo)
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener('load', startGame)
